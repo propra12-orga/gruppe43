160591f0bcb4d3aea8cfd5b324f9944244143cb1
Tobias Laatsch <t.f.laatsch@googlemail.com>
2012-05-09 19:14:14 +0200

    Methode zum Laden der Texture verbessert

--- Bombi/src/bombi/Texture.java
+++ Bombi/src/bombi/Texture.java
@@ -1,43 +1,39 @@
 package bombi;
 
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 
-import javax.swing.ImageIcon;
+import javax.imageio.ImageIO;
 
 /**
- * Zweck dieser Klasse ist, alle (?) verwendeten Texturen einzulesen
- * und diese als statische Objekte bereitzustellen, damit diese zB
- * in BombermanLevel gezeichnet werden können.
+ * Zweck dieser Klasse ist, alle (?) verwendeten Texturen einzulesen und diese als statische Objekte bereitzustellen, damit diese zB in BombermanLevel
+ * gezeichnet werden können.
  * 
  * @author tobi
- *
+ * 
  */
 public class Texture {
-    private static final String IMGURL = "/texture.png"; 
-    
+    private static final String IMGURL = "/texture.png";
+
     /**
-     * Statische Methode zum Einlesen eines Bildes, welches durch url gegeben
-     * ist. Das Bild wird als BufferedImage zurück gegeben
+     * Statische Methode zum Einlesen eines Bildes, welches durch url gegeben ist. Das Bild wird als BufferedImage zurück gegeben
      */
-    private static BufferedImage getBufferedImage(String url){
-        Image img = new ImageIcon(new Texture().getClass().getResource(url)).getImage();
-        BufferedImage bfImg = new BufferedImage(img.getWidth(null),
-                                               img.getHeight(null),
-                                               BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g = bfImg.createGraphics();
-        g.drawImage(img,0,0,null);
-        g.dispose();
-        return bfImg;}
-    
-    //benutzt obige Methode, um die Texturen einzulesen
-    private static final BufferedImage TEXTURE = Texture.getBufferedImage(IMGURL);
-    
+    private static BufferedImage getBufferedImage() {
+        try {
+            return ImageIO.read(new Texture().getClass().getResource(IMGURL));
+        } catch (IOException e) {
+            System.err.println("Fehler beim Laden der Texturen.");
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // benutzt obige Methode, um die Texturen einzulesen
+    private static final BufferedImage TEXTURE = getBufferedImage();
+
     /**
-     * WICHTIGER TEIL HIER! Bisher werden GRASS, STONE und BEDROCK aus der Datei
-     * texture.png erzeugt, welche 256x256 Pixel groß ist. Hierüber können einfach
+     * WICHTIGER TEIL HIER! Bisher werden GRASS, STONE und BEDROCK aus der Datei texture.png erzeugt, welche 256x256 Pixel groß ist. Hierüber können einfach
      * andere Texturen erzeugt werden, mit der folgenden Syntax:
      * 
      * public static final Texture *NAME* = new Texture(startXPixel,startYPixel,Breite,Höhe);
@@ -45,22 +41,22 @@ public class Texture {
      * (startXPixel,startYPixel) ist hierbei der linke obere Pixel in der Datei texture.png
      * 
      * texture.png befindet sich in Bombi/img/texture.png
-     **/   
-    
-    //tatsächlicher Zweck dieser Klasse.. Bereitstellung der Texturen =)
-    public static final Texture GRASS = new Texture(0,0,32,32);
-    public static final Texture STONE = new Texture(32,0,32,32);
-    public static final Texture BEDROCK = new Texture(64,0,32,32);
-    
+     **/
+
+    // tatsächlicher Zweck dieser Klasse.. Bereitstellung der Texturen =)
+    public static final Texture GRASS = new Texture(0, 0, 32, 32);
+    public static final Texture STONE = new Texture(32, 0, 32, 32);
+    public static final Texture BEDROCK = new Texture(64, 0, 32, 32);
+
     private BufferedImage texture;
-    
-    public Texture(){}//dummy-constructor für obige Methode.. das muss besser gehn!
-    
-    public Texture(int sx, int sy, int width, int height){
+
+    public Texture() {}// dummy-constructor für obige Methode.. das muss besser gehn!
+
+    public Texture(int sx, int sy, int width, int height) {
         texture = TEXTURE.getSubimage(sx, sy, width, height);
     }
-    
-    public void draw(int px, int py, int width, int height, Graphics g){
+
+    public void draw(int px, int py, int width, int height, Graphics g) {
         g.drawImage(texture, px, py, width, height, null);
     }
 }
