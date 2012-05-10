dca896ca6cc0f90396e72dfc095611d62e96ba85
Tobias Laatsch <t.f.laatsch@googlemail.com>
2012-05-10 15:52:10 +0200

    erster Versuch eines Spieleloops

--- Bombi/src/bombi/BombiGui.java
+++ Bombi/src/bombi/BombiGui.java
@@ -3,22 +3,29 @@ package bombi;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
-public class BombiGui extends JComponent {
+public class BombiGui extends JComponent implements Runnable{
 
 	BombermansBomben Bombe1;
     BombermanLevel bLevel;
     private static final int WIDTH = 840;
     private static final int HEIGHT = 600;
+    
+    private static final long SECOND = 1000000000;
+    private static final long SLEEPTIME = SECOND/60;
+    
+    private boolean running = true;
     private Image dbImage;
-    private Graphics dbg;
+    private Graphics2D dbg;
     int x = 0;
     int y = 0;
     int radius = 36;
@@ -82,24 +89,28 @@ public class BombiGui extends JComponent {
     /**
      * Double Buffering wobei ein Image erstellt wird und im Hintergrund das nächste Bild gemalt wird.
      */
-    public void paint(Graphics g) {
-        dbImage = createImage(getWidth(), getHeight());
-        dbg = dbImage.getGraphics();
-        paintComponent(dbg);
-        g.drawImage(dbImage, 0, 0, this);
-    }
-
-    @Override
-    public void paintComponent(Graphics g) {
-        super.paintComponents(g);
-        bLevel.draw(g);
+    public void paintBuffer() {
+        if(dbImage == null){
+        	dbImage = new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_ARGB);
+        	dbg = (Graphics2D) dbImage.getGraphics();
+        	}
+        
+        // zeichne das Lvel
+        bLevel.draw(dbg);
+        
         // Eine kleine rote Kugel die zum testen der
         // Bewegung und des Double Bufferings gedacht ist
+        dbg.setColor(Color.RED);
+        dbg.fillOval(x, y, radius, radius);
+        
+        // zeichne die Bombe
         if (Bombe1!= null){
-        Bombe1.draw(g);}
-        g.setColor(Color.RED);
-        g.fillOval(x, y, radius, radius);
-        repaint();
+        	Bombe1.draw(dbg);}
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(dbImage, 0, 0, this);        
     }
 
     /**
@@ -107,7 +118,6 @@ public class BombiGui extends JComponent {
      * 
      * @return: Dimension WIDTH x HEIGHT, welche fest codiert sind.
      */
-    @Override
     public Dimension getPreferredSize() {
         return new Dimension(WIDTH, HEIGHT);
     }
@@ -115,8 +125,7 @@ public class BombiGui extends JComponent {
     /**
      * Startet das Programm ;-)
      * 
-     * @param args
-     *            : Bisher werden alle Parameter ignoriert.
+     * @param args Bisher werden alle Parameter ignoriert.
      */
     public static void main(String[] args) {
         // Fenster erstellen
@@ -124,12 +133,54 @@ public class BombiGui extends JComponent {
         frame.setTitle("Bombi");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        frame.add(new BombiGui());
+        BombiGui bGui = new BombiGui();
+        
+        frame.add(bGui);      
         frame.pack(); // passt die Größe dem Inhalt an
 
         // zentriert das Fenster
         frame.setLocationRelativeTo(null);
 
         frame.setVisible(true);
+
+        new Thread(bGui).start();
     }
+
+    /**
+     * Startet den Spieleloop. 
+     */
+	public void run() {
+		long beforeUpdate, updateTime;
+		while(running){
+			
+			beforeUpdate = System.currentTimeMillis();
+			
+			
+			//update alle Objekte, Spieler etc
+			bombermanUpdate();
+			
+			//zeichne alle Objekte auf den Buffer
+			paintBuffer();
+			//zeichne den Buffer sichtbar fuer den Nutzer
+			repaint();
+			
+			
+			updateTime = System.currentTimeMillis() - beforeUpdate;
+			
+			//warte ein wenig....
+			try {
+				Thread.sleep((SLEEPTIME)/1000000);
+			}
+			catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
+	}
+	
+	public void bombermanUpdate(){
+		//bisher sind keine updatebaren Objekte da
+	}
 }
