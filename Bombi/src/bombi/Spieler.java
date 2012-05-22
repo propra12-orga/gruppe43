787a85b76344c5717b70ccc708372c40cada5724
Abuubaida <abuubaida@abuubaidas-macbook.local>
2012-05-22 12:34:59 +0200

    spielerklasse geaendert

--- Bombi/src/bombi/Spieler.java
+++ Bombi/src/bombi/Spieler.java
@@ -1,39 +1,101 @@
 package bombi;
 
+import java.awt.Graphics;
 import java.awt.Graphics2D;
 
 public class Spieler {
-
-    private int health = 1; // Zustand f√ºr abfrage ob Spieler verloren(=0)hat
-
-    private static int px, py, width, height;
-
-    public Spieler() {
-        px = py = 0;
+	
+	BombermanLevel l;
+	
+	Graphics g;
+    private int health = 1; // Zustand für abfrage ob Spieler verloren(=0)hat
+    private static int x, y, width, height;
+    Spieler s;
+    Texture t;
+    
+    public Spieler(BombermanLevel l) {
+    	this.l=l;
+    	
+        x = y = 0;
         width = height = 40;
     }
-
+ 
     public void draw(Graphics2D g) {
         if (health == 1) {
-            Texture.SPIELER.draw(px, py, width, height, g);
+            Texture.SPIELER1.draw(x, y, width, height, g);
         }
-
     }
-
-    public void moveLeft() {
-        px -= 5;
+    
+    public void stopStein() {
+    	
+    
+    	
+    	if((l.width==0)||(INDESTRUCTIBLE==2)){
+    		x=x;
+    		y=y;
+    	}
     }
-
-    public void moveRight() {
-        px += 5;
+   
+        
+    public void move(){
+    	if(x < 0)
+            x = 0;
+        if(x > 800)
+            x = 800;
+        if(y < 0)
+            y = 0;
+        if(y > 560)
+            y = 560;
     }
+   
+    public void Direction(int xdir,int ydir){
+        
+    	x += xdir;
+        y += ydir;
+        move();
+        
+    } 
+   
+        
 
-    public void moveUp() {
-        py -= 5;
+       
+        
+        public void verloren(){
+        	if(health == 0){
+        		x=0;
+        		y=0;
+        	}
+        	
+        	
+        }
+        /*
+        public void keyReleased(KeyEvent e){
+            int keyCode = e.getKeyCode();
+            if(keyCode == e.VK_LEFT){
+                setXDirection(-32,0);
+            }
+            if(keyCode == e.VK_RIGHT){
+                setXDirection(+32,0);
+            }
+            if(keyCode == e.VK_UP){
+                setXDirection(0,-32);
+            }
+            if(keyCode == e.VK_DOWN){
+                setXDirection(0,+32);
+            }
+           
+        }*/
+  
+ 
+  
+    public void run(){
+        try{
+            while(true){
+                move();
+                Thread.sleep(60);
+            }
+        }catch(Exception e){System.err.println(e.getMessage());}
     }
-
-    public void moveDown() {
-        py += 5;
-    }
-
+    
 }
+
